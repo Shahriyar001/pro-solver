1e9b7dbdefeb824d424a99e2374db8b582e1edec
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-07-29 22:41:57 +0600

    added 2 solved problem

--- js/test.js
+++ js/test.js
@@ -1,2 +1,22 @@
-console.log("hello world");
-console.log(" world");
+// problem: 1
+// Reverse a String
+// Problem: Write a function that reverses a given string.
+
+function reverseString(str) {
+  const result = str.split("").reverse().join("");
+  console.log(result);
+  return result;
+}
+
+// reverseString("level1");
+
+// Problem 2: Find the Largest Number
+// Problem: Write a function that finds the largest number in an array of numbers.
+
+function findLargestNumber(arr) {
+  const result = Math.max(...arr);
+  console.log(result);
+  return result;
+}
+
+// findLargestNumber([1, 2, 3, 4, 15]);
