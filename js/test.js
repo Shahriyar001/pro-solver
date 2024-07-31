a6cd0bbfad37a79a6f44814747712e26fe2903f3
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-07-31 23:44:58 +0600

    Palindrome Checker problem added

--- js/test.js
+++ js/test.js
@@ -30,3 +30,13 @@ for (let i = 1; i <= 100; i++) {
   if (i % 5 === 0) output += "Buzz";
   console.log(output || i);
 }
+
+// problem: 4
+// Palindrome Checker
+
+function isPalindrome(str) {
+  const cleanedStr = str.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+  return cleanedStr === cleanedStr.split("").reverse().join("");
+}
+
+console.log(isPalindrome("A man, a plan, a canal, Panama")); // true
